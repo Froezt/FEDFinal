305179a37403f8e5670a335c6ad3c5c55657fad1
Froezt <36000801+Froezt@users.noreply.github.com>
2020-01-21 17:52:02 +0100

    Add files via upload

--- java/java.js
+++ java/java.js
@@ -1,9 +1,20 @@
 var x = document.getElementById("myTopnav");
 
+
 function myFunction() {
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
+
+function check() {
+        document.querySelector("#check").style.display = "inline-block";
+        document.getElementById("laser1").classList.add("laser1");
+        document.getElementById("laser2").classList.add("laser2");
+        document.getElementById("audio").play();
+}
+
+document.querySelector("#stuurknop").addEventListener("click", check);
+
